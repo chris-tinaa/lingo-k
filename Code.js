5a0ec246240dd304aad787f9de4780622520da7f
chris-tinaa <chrislinlin.cl@gmail.com>
2023-02-09 11:38:07 +0700

    api connected to spreadsheet

--- Code.js
+++ Code.js
@@ -7,122 +7,137 @@ const conversations = doc.getSheetByName('conversations');
 const user = doc.getSheetByName("user");
 
 function doGet(e) {
-    let response;
-    const action = e.parameter.action;
-    const whatsapp = e.parameter.whatsapp;
-    const user_data = getDataByWhatsapp(whatsapp, user);
+  let response;
+  const action = e.parameter.action;
+  const whatsapp = e.parameter.whatsapp;
+  const user_data = getDataByWhatsapp(whatsapp, user);
 
-    if (action == 'start-registration') {
-      const id = generateNewId(user);
-      const name = e.parameter.name;
-      let data, message;
+  if (action == 'start-registration') {
+    const id = generateNewId(user);
+    const name = e.parameter.name;
+    let isSuccess, data, message;
 
-      if (user_data == null) {
-        user.appendRow([id, whatsapp, name, 0, 0, 0, 0]);
-        data = getDataById(id, user);
-        message = 'Success!';
-      } else {
-        data = null;
-        message = 'Failed. Your WhatsApp number is already signed up.'
-      }
-
-      response = {
-        success: true,
-        data: data,
-        message: message
-      }
-    } else if (action == 'get-vocab') {
-      let vocab_set = [];
-
-      if (!topik_i_vocab_finished(user_data)) {
-
-        const n = 5;
-        vocab_set = vocab_set.concat(getAllData(topik_i_vocab).slice(user_data.topik_i_vocab_count, user_data.topik_i_vocab_count+n));
-
-        user_data.topik_i_vocab_count = user_data.topik_i_vocab_count + n;
-        updateDataById(user_data, user);
-
-      } else if (!topik_ii_vocab_finished(user_data)){
-        
-        const n = 5;
-        vocab_set = vocab_set.concat(getAllData(topik_ii_vocab).slice(user_data.topik_ii_vocab_count, user_data.topik_ii_vocab_count+n));
-        
-        user_data.topik_ii_vocab_count = user_data.topik_ii_vocab_count + n;
-        updateDataById(user_data, user);
-
-      } else if (!common_vocab_finished(user_data)) {
-
-        const n = 5;
-        vocab_set = vocab_set.concat(getAllData(common_vocab).slice(user_data.common_vocab_count, user_data.common_vocab_count+n));
-        
-        user_data.common_vocab_count = user_data.common_vocab_count + n;
-        updateDataById(user_data, user);
-
-      } else {
-        vocab_set = get_vocab_review(user_data);
-      }
-
-      response = {
-        success: true,
-        data: vocab_set,
-        message: "Success to get vocabularies."
-      }
-
-    } else if (action == 'get-vocab-review') {
-
-      let vocab_set = get_vocab_review(user_data);
-
-      response = {
-        success: true,
-        data: vocab_set,
-        message: "Success to get vocabularies review."
-      }
-    } else if (action == 'get-grammar') {
-
-      let grammar_set;
-
-      if (!grammar_finised(user_data)) {
-        
-        grammar_set = {
-          grammar: getDataById(user_data.conv_title_count + 1, conversation_title),
-          example: getConversations(user_data.conv_title_count + 1, conversations) 
-        };
-
-        user_data.conv_title_count = user_data.conv_title_count + 1;
-        updateDataById(user_data, user);
-      
-      } else {
-
-        grammar_set = get_grammar_review(user_data);
-
-      }
-
-      response = {
-          success: true,
-          data: grammar_set,
-          message: "Success to get grammar."
-      }
-
-    } else if (action == 'get-grammar-review') {
-
-      let grammar_set = get_grammar_review(user_data);
-
-      response = {
-        success: true,
-        data: grammar_set,
-        message: "Success to get grammar review."
-      }
-    } else if (action == 'stop-subscription') {
-      
-      deleteUserByWhastsapp(whatsapp);
-
-      response = {
-        success: true,
-        message: "Success to delete user."
-      }
+    if (user_data == null) {
+      user.appendRow([id, whatsapp, name, 0, 0, 0, 0]);
+      data = getDataById(id, user);
+      message = 'Success!';
+      isSuccess = true;
+    } else {
+      data = null;
+      message = 'Failed. Your WhatsApp number is already signed up.'
+      isSuccess = false;
     }
 
-    return ContentService.createTextOutput(JSON.stringify(response)).setMimeType(ContentService.MimeType.JSON);
+    response = {
+      success: isSuccess,
+      data: data,
+      message: message
+    }
+  } else if (action == 'get-vocab') {
+    let vocab_set = [];
+    let isSuccess = true;
+
+    if (!topik_i_vocab_finished(user_data)) {
+
+      const n = 5;
+      vocab_set = vocab_set.concat(getAllData(topik_i_vocab).slice(user_data.topik_i_vocab_count, user_data.topik_i_vocab_count + n));
+
+      user_data.topik_i_vocab_count = user_data.topik_i_vocab_count + n;
+      updateDataById(user_data, user);
+
+    } else if (!topik_ii_vocab_finished(user_data)) {
+
+      const n = 5;
+      vocab_set = vocab_set.concat(getAllData(topik_ii_vocab).slice(user_data.topik_ii_vocab_count, user_data.topik_ii_vocab_count + n));
+
+      user_data.topik_ii_vocab_count = user_data.topik_ii_vocab_count + n;
+      updateDataById(user_data, user);
+
+    } else if (!common_vocab_finished(user_data)) {
+
+      const n = 5;
+      vocab_set = vocab_set.concat(getAllData(common_vocab).slice(user_data.common_vocab_count, user_data.common_vocab_count + n));
+
+      user_data.common_vocab_count = user_data.common_vocab_count + n;
+      updateDataById(user_data, user);
+
+    } else {
+      vocab_set = get_vocab_review(user_data);
+    }
+
+    response = {
+      success: isSuccess,
+      data: vocab_set,
+      message: "Success to get vocabularies."
+    }
+
+  } else if (action == 'get-vocab-review') {
+
+    let vocab_set = get_vocab_review(user_data);
+    let isSuccess = true;
+
+    response = {
+      success: isSuccess,
+      data: vocab_set,
+      message: "Success to get vocabularies review."
+    }
+  } else if (action == 'get-grammar') {
+
+    let grammar_set;
+    let isSuccess = true;
+
+    if (!grammar_finised(user_data)) {
+
+      grammar_set = {
+        grammar: getDataById(user_data.conv_title_count + 1, conversation_title),
+        example: getConversations(user_data.conv_title_count + 1, conversations)
+      };
+
+      user_data.conv_title_count = user_data.conv_title_count + 1;
+      updateDataById(user_data, user);
+
+    } else {
+
+      grammar_set = get_grammar_review(user_data);
+
+    }
+
+    response = {
+      success: isSuccess,
+      data: grammar_set,
+      message: "Success to get grammar."
+    }
+
+  } else if (action == 'get-grammar-review') {
+
+    let grammar_set = get_grammar_review(user_data);
+    let isSuccess = true;
+
+    response = {
+      success: isSuccess,
+      data: grammar_set,
+      message: "Success to get grammar review."
+    }
+  } else if (action == 'stop-subscription') {
+
+    let isSuccess, message;
+
+    if (user_data != null) {
+      deleteUserByWhastsapp(whatsapp);
+      message = 'Success to delete user.'
+      isSuccess = true;
+    } else {
+      message = 'Failed. Your number hasn\'t been signed up yet!';
+      isSuccess = false;
+    }
+
+    response = {
+      success: isSuccess,
+      message: message
+    }
+  }
+
+  return ContentService.createTextOutput(JSON.stringify(response)).setMimeType(ContentService.MimeType.JSON);
 }
 
 function deleteUserByWhastsapp(whatsapp) {
@@ -133,9 +148,9 @@ function deleteUserByWhastsapp(whatsapp) {
   var rowsDeleted = 0;
   for (var i = 0; i <= numRows - 1; i++) {
     var row = values[i];
-    if (row[1] == whatsapp || row[1] == '') { 
+    if (row[1] == whatsapp || row[1] == '') {
       // This searches all cells in columns A (change to row[1] for columns B and so on) and deletes row if cell is empty or has value 'delete'.
-      user.deleteRow((parseInt(i)+1) - rowsDeleted);
+      user.deleteRow((parseInt(i) + 1) - rowsDeleted);
       rowsDeleted++;
     }
   }
@@ -148,14 +163,14 @@ function get_grammar_review(user_data) {
     grammar = getRandomItem(getAllData(conversation_title));
     example = getConversations(grammar.id, conversations);
   } else {
-    let grammars = getAllData(conversation_title).slice(0,user_data.conv_title_count);
+    let grammars = getAllData(conversation_title).slice(0, user_data.conv_title_count);
 
     grammar = getRandomItem(grammars);
     example = getConversations(grammar.id, conversations);
   }
-  
+
   return {
-    "grammar": grammar, 
+    "grammar": grammar,
     "example": example
   };
 }
@@ -177,24 +192,24 @@ function get_vocab_review(user_data) {
 
     const n = 10;
     vocab_set = vocab_set
-      .concat(getAllData(topik_i_vocab))      
+      .concat(getAllData(topik_i_vocab))
       .concat(getAllData(topik_ii_vocab))
-      .concat(getAllData(common_vocab).slice(0,user_data.common_vocab_count));
+      .concat(getAllData(common_vocab).slice(0, user_data.common_vocab_count));
     vocab_set = getMultipleRandomItem(vocab_set, n);
 
   } else if (topik_i_vocab_finished(user_data)) {
 
     const n = 10;
     vocab_set = vocab_set
-      .concat(getAllData(topik_i_vocab))      
-      .concat(getAllData(topik_ii_vocab).slice(0,user_data.topik_ii_vocab_count));
+      .concat(getAllData(topik_i_vocab))
+      .concat(getAllData(topik_ii_vocab).slice(0, user_data.topik_ii_vocab_count));
     vocab_set = getMultipleRandomItem(vocab_set, n);
 
   } else {
 
     const n = 10;
     vocab_set = vocab_set
-      .concat(getAllData(topik_i_vocab).slice(0,user_data.topik_i_vocab_count));
+      .concat(getAllData(topik_i_vocab).slice(0, user_data.topik_i_vocab_count));
     vocab_set = getMultipleRandomItem(vocab_set, n);
 
   }
@@ -209,23 +224,23 @@ function getMultipleRandomItem(arr, num) {
 }
 
 function getRandomItem(arr) {
-  return arr[Math.floor(Math.random()*arr.length)];
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function topik_i_vocab_finished(user_data){
-  return ((topik_i_vocab.getLastRow()-1) == user_data.topik_i_vocab_count);
+function topik_i_vocab_finished(user_data) {
+  return ((topik_i_vocab.getLastRow() - 1) == user_data.topik_i_vocab_count);
 }
 
-function topik_ii_vocab_finished(user_data){
-  return ((topik_ii_vocab.getLastRow()-1) == user_data.topik_ii_vocab_count);
+function topik_ii_vocab_finished(user_data) {
+  return ((topik_ii_vocab.getLastRow() - 1) == user_data.topik_ii_vocab_count);
 }
 
-function common_vocab_finished(user_data){
-  return ((common_vocab.getLastRow()-1) == user_data.common_vocab_count);
+function common_vocab_finished(user_data) {
+  return ((common_vocab.getLastRow() - 1) == user_data.common_vocab_count);
 }
 
-function grammar_finised(user_data){
-  return ((conversation_title.getLastRow()-1) == user_data.conv_title_count);
+function grammar_finised(user_data) {
+  return ((conversation_title.getLastRow() - 1) == user_data.conv_title_count);
 }
 
 function updateDataById(new_data, sheet) {
@@ -247,24 +262,24 @@ function getColumnValues(columnName, sheet) {
 
 
 function getAllData(sheet) {
-    const data = [];
-    const rlen = sheet.getLastRow();
-    const clen = sheet.getLastColumn();
+  const data = [];
+  const rlen = sheet.getLastRow();
+  const clen = sheet.getLastColumn();
 
-    const rows = sheet.getRange(1, 1, rlen, clen).getValues();
+  const rows = sheet.getRange(1, 1, rlen, clen).getValues();
 
-    for (let i = 0; i < rows.length; i++) {
-      const dataRow = rows[i];
-      let record = {};
-      for (let j=0; j < clen; j++) {
-        record[rows[0][j]] = dataRow[j];
-      }
-      if (i > 0) {
-        data.push(record);
-      }
+  for (let i = 0; i < rows.length; i++) {
+    const dataRow = rows[i];
+    let record = {};
+    for (let j = 0; j < clen; j++) {
+      record[rows[0][j]] = dataRow[j];
     }
+    if (i > 0) {
+      data.push(record);
+    }
+  }
 
-    return data;
+  return data;
 }
 
 function generateNewId(sheet) {
@@ -272,25 +287,25 @@ function generateNewId(sheet) {
     return 1;
   }
 
-  return  getAllData(sheet).reduce((max, row) => max.id > row.id ? max : row).id + 1;
+  return getAllData(sheet).reduce((max, row) => max.id > row.id ? max : row).id + 1;
 }
 
 function getDataById(id, sheet) {
-  let data = getAllData(sheet).find(function(element) {
+  let data = getAllData(sheet).find(function (element) {
     return element['id'] == id;
-  }) 
+  })
   return data
 }
 
 function getDataByWhatsapp(whatsapp, sheet) {
-  let data = getAllData(sheet).find(function(element) {
+  let data = getAllData(sheet).find(function (element) {
     return element['number'] == whatsapp;
   })
   return data;
 }
 
 function getConversations(title_id, sheet) {
-  let data = getAllData(sheet).filter(function(element) {
+  let data = getAllData(sheet).filter(function (element) {
     return element['title_id'] == title_id;
   });
   return data;
--- index.js
+++ index.js
@@ -1,58 +1,178 @@
-const {DisconnectReason, useMultiFileAuthState} = require('@adiwajshing/baileys');
+const { DisconnectReason, useMultiFileAuthState } = require('@adiwajshing/baileys');
+const { default: axios } = require('axios');
+const { text } = require('body-parser');
 const makeWASocket = require('@adiwajshing/baileys').default;
 
-const startSock = async() => {
+const BASE_URL = "https://script.google.com/macros/s/AKfycbwl-xKtkanJDeX-vJbPdMrq1IllvMnz-3u8V2utNd5jCqD3fxP1oVz5IqNiBJCaJgpNlQ/exec?";
+const TRANSLATOR_URL = "https://text-translator2.p.rapidapi.com/translate";
 
-    try {
-        // file for whatsapp auth when scanning
-        const {state, saveState} = await useMultiFileAuthState('auth.json');
-        
-        const sock = makeWASocket({
-            printQRInTerminal: true,
-            auth: state
-        });
 
-        // error di sini
-        sock.ev.on('creds.update', saveState);
 
-        console.log("ngetest: state ", state);
-        console.log("ngetest: savestate ", saveState);
-        
-        sock.ev.on('connection.update', (update, connection2) => {
-            let _a, _b;
-            let connection = update.connection, lastDisconnect = update.lastDisconnect;
+const startSock = async () => {
 
-            if (connection === 'close') {
-                console.log("ngetest: connection close");
+    // file for whatsapp auth when scanning
+    const { state, saveCreds } = await useMultiFileAuthState('auth.json');
 
-                const shouldReconnect = ((_b = (_a = lastDisconnect.error) === null
-                                            || _a === void 0 ? void 0 : _a.output) === null
-                                            || _b === void 0 ? void 0 : _b.statusCode) !== DisconnectReason.loggedOut
-                console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect)
-                // reconnect if not logged out
-                if(shouldReconnect) {
-                    console.log("ngetest: reconnect");
+    const sock = makeWASocket({
+        printQRInTerminal: true,
+        auth: state
+    });
 
-                    startSock();
-                }
-            } else if(connection === 'open') {
-                console.log("ngetest: open");
+    sock.ev.on('creds.update', saveCreds);
 
-                console.log('connection : ' + JSON.stringify(connection));
+    sock.ev.on('connection.update', (update, connection2) => {
+        let _a, _b;
+        let connection = update.connection, lastDisconnect = update.lastDisconnect;
+
+        if (connection === 'close') {
+            console.log("ngetest: connection close");
+
+            const shouldReconnect = ((_b = (_a = lastDisconnect.error) === null
+                || _a === void 0 ? void 0 : _a.output) === null
+                || _b === void 0 ? void 0 : _b.statusCode) !== DisconnectReason.loggedOut
+            console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect)
+            // reconnect if not logged out
+            if (shouldReconnect) {
+                console.log("ngetest: reconnect");
+
+                startSock();
             }
-        });
+        } else if (connection === 'open') {
+            console.log("ngetest: open");
 
-        sock.ev.on('messages.upsert', async m => {
-            // console.log(JSON.stringify(m, undefined, 2))
+            console.log('connection : ' + JSON.stringify(connection));
+        }
+    });
 
-            // console.log('replying to', m.messages[0].key.remoteJid)
-            // await sock.sendMessage(m.messages[0].key.remoteJid!, { text: 'Hello there!' })
-        });
-    } catch (error) {
-        console.log("ngetest: eror nih");
+    sock.ev.on('messages.upsert', async m => {
+        // LOGIC untuk BOT WhatsApp
+        const msg = m.messages[0];
+        console.log("There's a message: " + JSON.stringify(msg));
 
-        console.error("Error message: ", error);
-    }
+        if (!msg.key.fromMe && m.type === 'notify') {
+            if (msg.key.remoteJid.includes('@s.whatsapp.net')) {
+
+                if (msg.message.templateButtonReplyMessage?.selectedId == 'start-subscription' || msg.message.conversation.toLowerCase().includes('\\start')) {
+
+                    const sentMsg = await sock.sendMessage(msg.key.remoteJid, { text: 'Great! What\'s your name?\n\n*Write your answer in this format:*\nMy name is _your-name-here_' }, { quoted: msg });
+
+                } else if (msg.message.templateButtonReplyMessage?.selectedId == 'info' || msg.message.conversation.toLowerCase().includes('\\info')) {
+
+                    const sentMsg = await sock.sendMessage(msg.key.remoteJid, { text: `Subscription is _free_!\n\nHere are what I can do for you:\n\n🍦*VOCABULARY*\n[1] 1671 words, level TOPIK I\n[2] 2662 words, level TOPIK II\n[3] 5667 common vocabs\n\nI send 5 vocabs at 9 AM, 11 AM, 1 PM, 3 PM, and 5 PM everyday.\n\nAt 6 PM and 7 PM, I send 10 random vocabs you've already learned to review.\n\n\n🍦*GRAMMAR*\n[1] 849 grammars\n[2] Examples for each grammar\n\nI send 2 grammars and example for each everyday at 8 PM and 9 PM. \n\n\n🍦*TRANSLATOR*\nUse these format to translate:\n[1] Eng to Kor: _write-anything-here_\n[2] Kor to Eng: _write-anything-here_\n\nAnyway, you can use translation feature without subscription.\n\n-------\n\nJust answer *\\start* if you want to subscribe! :)` }, { quoted: msg });
+
+                } else if (msg.message.conversation.toLowerCase().includes('my name is')) {
+                    const name = msg.message.conversation.toLowerCase()
+                        .replace("my name is ", "")
+                        .replace(/(?:\r\n|\r|\n)/g, "");
+
+                    axios.get(`${BASE_URL}action=start-subscription&whatsapp=${msg.key.remoteJid.replace("@s.whatsapp.net", "")}&name=${name}`).
+                        then((response) => {
+                            console.log("Start resp: " + JSON.stringify(response.data));
+                            let { success, data, message } = response.data;
+
+                            if (success) {
+                                let str = `${message} Hi ${name}, let's get your first lesson with Lingo K😄`;
+                                sock.sendMessage(msg.key.remoteJid, {
+                                    text: str
+                                });
+                            } else {
+                                sock.sendMessage(msg.key.remoteJid, {
+                                    text: `${message}☹`
+                                });
+                            }
+                        }).catch(function (error) {
+                            console.error(error);
+                        });;
+
+                } else if (msg.message.conversation.toLowerCase().includes('\\stop')) {
+
+                    axios.get(`${BASE_URL}action=stop-subscription&whatsapp=${msg.key.remoteJid.replace("@s.whatsapp.net", "")}`).
+                        then((response) => {
+                            console.log("Start resp: " + JSON.stringify(response.data));
+                            let { success, data, message } = response.data;
+
+                            if (success) {
+                                sock.sendMessage(msg.key.remoteJid, { text: 'Goodbye, it was fun with you!' }, { quoted: msg });
+                            } else {
+                                sock.sendMessage(msg.key.remoteJid, { text: 'You\'re not signed up yet.' }, { quoted: msg });
+                            }
+                        }).catch(function (error) {
+                            console.error(error);
+                        });;
+                } else if (msg.message.conversation.toLowerCase().includes('eng to kor')) {
+
+                    const text = msg.message.conversation.toLowerCase()
+                        .replace("eng to kor", "")
+                        .replace(":", "")
+                        .replace("=", "")
+                        .replace(/(?:\r\n|\r|\n)/g, "");
+
+                    const encodedParams = new URLSearchParams();
+                    encodedParams.append("source_language", "en");
+                    encodedParams.append("target_language", "ko");
+                    encodedParams.append("text", text);
+
+                    axios.request({
+                        method: 'POST',
+                        url: TRANSLATOR_URL,
+                        headers: {
+                            'content-type': 'application/x-www-form-urlencoded',
+                            'X-RapidAPI-Key': 'd7233a1200msh876cb9bc7677956p100679jsndf50e3a59333',
+                            'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+                        },
+                        data: encodedParams
+                    }).then(function (response) {
+                        console.log(response.data);
+                        sock.sendMessage(msg.key.remoteJid, { text: `${response.data.data.translatedText}` }, { quoted: msg });
+                    }).catch(function (error) {
+                        console.error(error);
+                    });
+                } else if (msg.message.conversation.toLowerCase().includes('kor to eng')) {
+                    const text = msg.message.conversation.toLowerCase()
+                        .replace("kor to eng", "")
+                        .replace(":", "")
+                        .replace("=", "")
+                        .replace(/(?:\r\n|\r|\n)/g, "");
+
+                    const encodedParams = new URLSearchParams();
+                    encodedParams.append("source_language", "ko");
+                    encodedParams.append("target_language", "en");
+                    encodedParams.append("text", text);
+
+                    axios.request({
+                        method: 'POST',
+                        url: TRANSLATOR_URL,
+                        headers: {
+                            'content-type': 'application/x-www-form-urlencoded',
+                            'X-RapidAPI-Key': 'd7233a1200msh876cb9bc7677956p100679jsndf50e3a59333',
+                            'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+                        },
+                        data: encodedParams
+                    }).then(function (response) {
+                        console.log(response.data);
+                        sock.sendMessage(msg.key.remoteJid, { text: `${response.data.data.translatedText}` }, { quoted: msg });
+                    }).catch(function (error) {
+                        console.error(error);
+                    });
+                } else {
+                    //send a template message with an image **attached**!
+                    const templateButtons = [
+                        { index: 1, quickReplyButton: { displayText: 'Start!😆', id: "start-subscription" } },
+                        { index: 2, quickReplyButton: { displayText: 'Info💬', id: "info" } },
+                    ]
+
+                    const templateMessage = {
+                        text: `안녕하세요!\n\nHola, I help people to learn Korean. I can send vocabs and grammar every several hours, everyday.\n\n_Just answer *\\start* if you want to use me! Or *\\info* for further detail_`,
+                        footer: 'Lingo K',
+                        templateButtons: templateButtons
+                    }
+
+                    const sendMsg = await sock.sendMessage(msg.key.remoteJid, templateMessage)
+                }
+
+            }
+        }
+    });
 }
 
 startSock();
